e14c4e03431a804a7cd298bf456d106f598b6f16
Hernan Antonio Martin Castillo <hernan@1001001.io>
2017-11-01 15:31:45 +0100

    new portfolio pictures, addes separator class and media queries

--- resources/views/homePartials/work.blade.php
+++ resources/views/homePartials/work.blade.php
@@ -1,92 +1,104 @@
 <!--WORK-->
 <section class="grey-bg mar-tm-10" id="work">
-  <div class="container">
+  <div class="container ">
     <div class="row">
-      <div class="col-md-3">
-        <h3 class="title-small">
-          <span>Work</span>
-        </h3>
-        <p class="content-detail">
-        My favourite programming language is PHP, I work the least in projects that include some kind \
-        of php framework like Laravel, Symfony or even Wordpress.
-        <br>
-        <br>
-        Picking up a new programming language or other framework is no problem.
-         I'm comfortable developing together with a team but can also work remotely.
-        </p>
-      </div>
-      <div class="col-md-9 content-right">
-        {{-- <!--PORTFOLIO IMAGE-->
-        <ul class="portfolio-image">
-          <li class="col-md-6">
-            <a href="images/bw-1.png"><img alt="image" src="{{asset('images/bw-1.png')}}">
-              <div class="decription-wrap">
-                <div class="image-bg">
-                   <p class="desc">My Resume</p>
-                </div>
-
-              </div>
+        <div class="col-md-7 col-sm-8 col-offset-sm-2 col-xs-8 col-offset-xs-2">
+            <a href="images/portfolio-1.jpg"><img alt="image" src="{{asset('images/portfolio-1.jpg')}}">
             </a>
-          </li>
-          <li class="col-md-6">
-            <a href="images/bw-2.png"><img alt="image" src="{{asset('images/bw-2.png')}}">
-              <div class="decription-wrap">
-                <div class="image-bg">
-                  <p class="desc">
-                    Pangirutan
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="col-md-6">
-            <a href="images/bw-3.png"><img alt="image" src="{{asset('images/bw-3.png')}}">
-              <div class="decription-wrap">
-                <div class="image-bg">
-                  <p class="desc">
-                    Black Mug
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="col-md-6">
-            <a href="images/bw-4.png"><img alt="image" src="{{asset('images/bw-4.png')}}">
-              <div class="decription-wrap">
-                <div class="image-bg">
-                  <p class="desc">
-                    Notebook Mockup
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="col-md-6">
-            <a href="images/bw-5.png"><img alt="image" src="{{asset('images/bw-5.png')}}">
-              <div class="decription-wrap">
-                <div class="image-bg">
-                  <p class="desc">
-                    Presentation
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="col-md-6">
-            <a href="images/bw-6.png"><img alt="image" src="{{asset('images/bw-6.png')}}">
-              <div class="decription-wrap">
-                <div class="image-bg">
-                  <p class="desc">
-                    Letter Branding
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!--/.PORTFOLIO IMAGE END--> --}}
-      </div>
+        </div>
+        <div class="col-md-5 col-sm-12 col-xs-12">
+            <h3 class="title-small"><span>Portfolio 01</span></h3>
+            <p class="content-detail no-pad">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+        </div>
     </div>
+
+    <div class="separator hidden-sm hidden-xs"></div>
+
+    <div class="row">
+        <div class="col-md-7 col-sm-8 col-offset-sm-2 col-xs-8 col-offset-xs-2">
+            <a href="images/portfolio-2.jpg"><img alt="image" src="{{asset('images/portfolio-2.jpg')}}">
+            </a>
+        </div>
+        <div class="col-md-5 col-sm-12 col-xs-12">
+            <h3 class="title-small"><span>Portfolio 02</span></h3>
+            <p class="content-detail no-pad">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+        </div>
+    </div>
+
+    <div class="separator hidden-sm hidden-xs"></div>
+
+    <div class="row">
+        <div class="col-md-7 col-sm-8 col-offset-sm-2 col-xs-8 col-offset-xs-2">
+            <a href="images/portfolio-3.jpg"><img alt="image" src="{{asset('images/portfolio-3.jpg')}}">
+            </a>
+        </div>
+        <div class="col-md-5 col-sm-12 col-xs-12">
+            <h3 class="title-small"><span>Portfolio 03</span></h3>
+            <p class="content-detail no-pad">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+        </div>
+    </div>
+
+    <div class="separator hidden-sm hidden-xs"></div>
+
+    <div class="row">
+        <div class="col-md-7 col-sm-8 col-offset-sm-2 col-xs-8 col-offset-xs-2">
+            <a href="images/portfolio-4.jpg"><img alt="image" src="{{asset('images/portfolio-4.jpg')}}">
+            </a>
+        </div>
+        <div class="col-md-5 col-sm-12 col-xs-12">
+            <h3 class="title-small"><span>Portfolio 04</span></h3>
+            <p class="content-detail no-pad">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+        </div>
+    </div>
+    {{-- <ul class="portfolio-image">
+
+      <li class="col-md-6">
+        <a href="images/portfolio-1.jpg"><img alt="image" src="{{asset('images/portfolio-1.jpg')}}">
+        </a>
+      </li>
+
+
+
+      <li class="col-md-6">
+        <a href="images/portfolio-1.jpg"><img alt="image" src="{{asset('images/portfolio-1.jpg')}}">
+        </a>
+      </li>
+
+      <li class="col-md-6">
+        <a href="images/portfolio-1.jpg"><img alt="image" src="{{asset('images/portfolio-1.jpg')}}">
+        </a>
+      </li>
+
+      <li class="col-md-6">
+        <a href="images/portfolio-1.jpg"><img alt="image" src="{{asset('images/portfolio-1.jpg')}}">
+        </a>
+      </li>
+
+    </ul> --}}
   </div>
 </section>
 <!--/.WORK END-->
